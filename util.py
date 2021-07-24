ffd86d0e0f8f05edf36988943eddd6b855478552
Vitor Rodarte <vitorricoy@gmail.com>
2021-07-24 19:14:31 -0300

    Alteração para usar np.isclose

--- util.py
+++ util.py
@@ -12,20 +12,20 @@ def pivotearElemento(tableau, linha, coluna):
 
 def igual(valor1, valor2):
     # Verifica se dois valores são iguais, considerando precisão de 7 casas decimais
-    return np.round(valor1, 7) == np.round(valor2, 7)
+    return np.all(np.isclose(valor1, valor2))
 
 def maiorIgual(valor1, valor2):
     # Verifica se o primeiro valor é maior ou igual ao segundo, considerando precisão de 7 casas decimais
-    return np.round(valor1, 7) >= np.round(valor2, 7)
+    return np.all(valor1 > valor2) or np.all(np.isclose(valor1, valor2))
 
 def menor(valor1, valor2):
     # Verifica se o primeiro valor é menor que o segundo, considerando precisão de 7 casas decimais
-    return np.round(valor1, 7) < np.round(valor2, 7)
+    return np.all(valor1 < valor2) and not np.all(np.isclose(valor1, valor2))
 
 def maior(valor1, valor2):
     # Verifica se o primeiro valor é maior que o segundo, considerando precisão de 7 casas decimais
-    return np.round(valor1, 7) > np.round(valor2, 7)
+    return np.all(valor1 > valor2) and not np.all(np.isclose(valor1, valor2))
 
 def menorIgual(valor1, valor2):
     # Verifica se o primeiro valor é menor ou igual ao segundo, considerando precisão de 7 casas decimais
-    return np.round(valor1, 7) <= np.round(valor2, 7)
+    return np.all(valor1 < valor2) or np.all(np.isclose(valor1, valor2))
