b3f75e8310cb8234dacd9f1564242e8f7376b95b
Vitor Rodarte <vitorricoy@gmail.com>
2021-07-25 12:31:38 -0300

    Corrigido comentário

--- util.py
+++ util.py
@@ -11,21 +11,21 @@ def pivotearElemento(tableau, linha, coluna):
     return tableau
 
 def igual(valor1, valor2):
-    # Verifica se dois valores são iguais, considerando precisão de 7 casas decimais
+    # Verifica se dois valores são iguais, considerando precisão padrão do numpy.isclose
     return np.all(np.isclose(valor1, valor2))
 
 def maiorIgual(valor1, valor2):
-    # Verifica se o primeiro valor é maior ou igual ao segundo, considerando precisão de 7 casas decimais
+    # Verifica se o primeiro valor é maior ou igual ao segundo, considerando precisão padrão do numpy.isclose
     return np.all(valor1 > valor2) or np.all(np.isclose(valor1, valor2))
 
 def menor(valor1, valor2):
-    # Verifica se o primeiro valor é menor que o segundo, considerando precisão de 7 casas decimais
+    # Verifica se o primeiro valor é menor que o segundo, considerando precisão padrão do numpy.isclose
     return np.all(valor1 < valor2) and not np.all(np.isclose(valor1, valor2))
 
 def maior(valor1, valor2):
-    # Verifica se o primeiro valor é maior que o segundo, considerando precisão de 7 casas decimais
+    # Verifica se o primeiro valor é maior que o segundo, considerando precisão padrão do numpy.isclose
     return np.all(valor1 > valor2) and not np.all(np.isclose(valor1, valor2))
 
 def menorIgual(valor1, valor2):
-    # Verifica se o primeiro valor é menor ou igual ao segundo, considerando precisão de 7 casas decimais
+    # Verifica se o primeiro valor é menor ou igual ao segundo, considerando precisão padrão do numpy.isclose
     return np.all(valor1 < valor2) or np.all(np.isclose(valor1, valor2))
